8b11cb219126bb19d7483ce8c384461cf24519ed
Kurt Stephens <ks.github@kurtstephens.com>
2014-03-25 09:26:45 -0500

    More portable.

--- malloc_debug.c
+++ malloc_debug.c
@@ -10,7 +10,7 @@ void *_debug_malloc(unsigned int x, const char *file, unsigned int line)
   fprintf(stderr, "%16s:%6u: malloc(%u)", file, line, x); fflush(stderr);
   p = malloc(x);
   if ( _malloc_debug )
-  fprintf(stderr, " => 0x%x\n", (unsigned int) p);
+    fprintf(stderr, " => 0x%lx\n", (unsigned long) (size_t) p);
   return p;
 }
 
@@ -18,15 +18,15 @@ void *_debug_realloc(void *x, unsigned int y, const char *file, unsigned int lin
 {
   void *p = realloc(x, y);
   if ( _malloc_debug )
-  fprintf(stderr, "%16s:%6u: realloc(0x%x, %u)", file, line, (unsigned int) x, y); fflush(stderr);
+  fprintf(stderr, "%16s:%6u: realloc(0x%lx, %u)", file, line, (unsigned long) (size_t) x, y); fflush(stderr);
   p = realloc(x, y);
   if ( _malloc_debug )
-  fprintf(stderr, " => 0x%x\n", (unsigned int) p);
+    fprintf(stderr, " => 0x%lx\n", (unsigned long) (size_t) p);
   return p;
 }
 void _debug_free(void *x, const char *file, unsigned int line)
 {
   if ( _malloc_debug )
-  fprintf(stderr, "%16s:%6u: free(0x%x)\n", file, line, (unsigned int) x);
+    fprintf(stderr, "%16s:%6u: free(0x%lx)\n", file, line, (unsigned long) (size_t) x);
   free(x);
 }
