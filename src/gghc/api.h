af2e6bf75a056d3fb505ea2991e834496dd24d4c
Kurt Stephens <ks.github@kurtstephens.com>
2014-03-27 09:32:48 -0500

    API is Contextual.

--- src/gghc/api.h
+++ src/gghc/api.h
@@ -10,40 +10,45 @@ extern "C" {
 
 #include <stddef.h>	/* size_t */
 
+#ifndef gghc_ctx
+typedef void *gghc_ctx;
+#define gghc_ctx gghc_ctx
+#endif
+
 #ifndef GGHCT
 typedef void *GGHCT;
 #endif
 
-typedef	void	(*gghc_funcp)(void);
+typedef	void (*gghc_funcp)(void);
 
 /* Begin/end module */
-void	gghc_begin_module(const char *module);
-void	gghc_end_module(const char *module);
+void	gghc_begin_module(gghc_ctx ctx, const char *module);
+void	gghc_end_module(gghc_ctx ctx, const char *module);
 
 /* Accept a #pragma gghc <str> */
-void	gghc_pragma(const char *file, unsigned int line, const char *str);
+void	gghc_pragma(gghc_ctx ctx, const char *file, unsigned int line, const char *str);
 
 /* Create a typedef */
-void gghc_typedef(const char *symbol, GGHCT ctype);
+void gghc_typedef(gghc_ctx ctx, const char *symbol, GGHCT ctype);
 
 /* Get a type by name */
-GGHCT gghc_type(const char *type_name);
+GGHCT gghc_type(gghc_ctx ctx, const char *type_name);
 
 /* Create a enum type */
-GGHCT gghc_enum_type(const char *enumname);
-void gghc_enum_type_element(GGHCT enumtype, const char *name, int enumvalue);
-void gghc_enum_type_end(GGHCT enumtype);
+GGHCT gghc_enum_type(gghc_ctx ctx, const char *enumname);
+void gghc_enum_type_element(gghc_ctx ctx, GGHCT enumtype, const char *name, int enumvalue);
+void gghc_enum_type_end(gghc_ctx ctx, GGHCT enumtype);
 
 /* Create a pointer type */
-GGHCT gghc_pointer_type(GGHCT elementtype);
+GGHCT gghc_pointer_type(gghc_ctx ctx, GGHCT elementtype);
 
 /* Create an array type */
-GGHCT gghc_array_type(GGHCT elementtype, int length);
+GGHCT gghc_array_type(gghc_ctx ctx, GGHCT elementtype, int length);
 
 /* Create a struct or union type */
-GGHCT gghc_struct_type(const char *struct_or_union, const char *structname, size_t _sizeof);
-void gghc_struct_type_element(GGHCT structtype, GGHCT elementtype, const char *elementname, gghc_funcp getfunc, gghc_funcp setfunc);
-void gghc_struct_type_end(GGHCT structtype);
+GGHCT gghc_struct_type(gghc_ctx ctx, const char *struct_or_union, const char *structname, size_t _sizeof);
+void gghc_struct_type_element(gghc_ctx ctx, GGHCT structtype, GGHCT elementtype, const char *elementname, gghc_funcp getfunc, gghc_funcp setfunc);
+void gghc_struct_type_end(gghc_ctx ctx, GGHCT structtype);
 
 /* Create a function type */
 #ifndef GGHCT_NULL
@@ -52,13 +57,13 @@ void gghc_struct_type_end(GGHCT structtype);
 #ifndef GGHCT_VARARGS
 #define GGHCT_VARARGS (GGHCT)-1
 #endif
-GGHCT gghc_function_type(GGHCT rtntype, ...);
+GGHCT gghc_function_type(gghc_ctx ctx, GGHCT rtntype, ...);
 
 /* Define a global */
-void gghc_global(const char *symbol, GGHCT ctype, void *address);
+void gghc_global(gghc_ctx ctx, const char *symbol, GGHCT ctype, void *address);
 
 /* Execute the initializers */
-void	gghc_init(void); 
+void	gghc_init(gghc_ctx ctx);
 
 #ifdef __cplusplus
 }
